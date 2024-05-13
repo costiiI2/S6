5ed8147314f7667f8b132afcf0f7c6903748b9fa
costi <costantino.cecchet@heig-vd.ch>
2024-05-13 10:41:08 +0200

    ff 15

--- SCF/LABO/L7/labo7/code/src_vhdl/neuron_combinational.vhd
+++ SCF/LABO/L7/labo7/code/src_vhdl/neuron_combinational.vhd
@@ -26,25 +26,56 @@ entity neuron is
 end neuron;
 
 architecture combinational of neuron is
-
     begin   
-
-    -- calcul combinatoire de la sortie
-    process(all)
-        -- on aloue 2 * datasize pour la somme
-        variable sum : unsigned((DATASIZE*2)-1 downto 0) := (others => '0');
-    begin
-        sum := (others => '0');
+        -- calcul combinatoire de la sortie
+        process(all)
+            -- on aloue 2 * datasize pour la somme
+            variable sum : unsigned((DATASIZE*2)-1 downto 0) := (others => '0');
+        begin
+            sum := (others => '0');
             for i in 0 to NBINPUTS-1 loop
                 sum := sum + unsigned(inputs_i(i)) * unsigned(weights_i(i));
             end loop;
             -- on mets la virgule au bon endroit par rapport a data size et notre calcul
             -- la virgule se trouve a la position COMMA_POS on doit donc tronquer les bits de COMMA_POS taille a droite et a gauche
             result_o <= std_logic_vector(sum(DATASIZE+COMMA_POS-1 downto COMMA_POS));
-      
-    end process;
+        end process;
+    
+        process(valid_o, ready_i)
+        begin
+            if valid_o = '1' and ready_i = '1' then
+                for i in 0 to DATASIZE-1 loop
+                    ready_o(i) <= '1';
+                end loop;
+            else
+                for i in 0 to DATASIZE-1 loop
+                    ready_o(i) <= '0';
+                end loop;
+            end if;
+        end process;
+    
+        process(valid_i)
+        begin
+            valid_o <= '0';
+            for i in valid_i'range loop
+                if valid_i(i) = '1' then
+                    valid_o <= '1';
+                    exit;
+                end if;
+            end loop;
+        end process;
 
-    ready_o <= valid_i;
-    valid_o <= ready_i;
-
+/*
+# +--------------------+
+# | FINAL REPORT       |
+# |--------------------+
+# | Nb warnings = 0
+# | Nb errors   = 1
+# |
+# | Verbosity level is : note
+# |
+# | END OF SIMULATION
+#    Time: 100 us  Iteration: 0  Instance: /neuron_tb/monitor
+# +--------------------+
+*/
 end combinational;
--- SCF/LABO/L7/labo7/code/src_vhdl/neuron_pipeline.vhd
+++ SCF/LABO/L7/labo7/code/src_vhdl/neuron_pipeline.vhd
@@ -27,6 +27,72 @@ end neuron;
 
 architecture pipeline of neuron is
 
-begin
+    begin
+          
+        process(clk_i, rst_i)
+                variable result_s : unsigned((DATASIZE*2)-1 downto 0) := (others => '0');
+                variable mult_s : unsigned((DATASIZE*2)-1 downto 0) := (others => '0');
+                variable all_ready :  natural range 0 to NBINPUTS := 0;
+                variable i : natural range 0 to NBINPUTS := 0;
 
+            begin
+                if rst_i = '1' then
+    
+                    for i in 0 to NBINPUTS-1 loop
+                        ready_o(i) <= '0';
+                    end loop;
+                    valid_o <= '0';
+                    result_s := (others => '0');
+                    mult_s := (others => '0');
+                    i :=0;
+                    all_ready := 0;
+    
+                elsif rising_edge(clk_i) then
+                    if(valid_o = '1' and ready_i = '1')then
+                        valid_o <= '0';
+                        result_s := (others => '0');
+                        all_ready := 0;
+                    end if;
+    
+                    if (valid_i(i) = '1' and valid_o /= '1') then
+                        mult_s := unsigned(inputs_i(i)) * unsigned(weights_i(i));
+                        result_s := result_s + mult_s;
+                        ready_o(i) <= '1'; 
+                        if all_ready /= NBINPUTS-1 then
+                            all_ready := all_ready + 1; 
+                        end if;
+                        
+                    else
+                        ready_o(i) <= '0';
+                    end if;
+    
+                    if (all_ready = NBINPUTS-1) then
+                        result_o <= std_logic_vector(result_s(DATASIZE + COMMA_POS-1 downto COMMA_POS));
+                        valid_o <= '1';
+                    end if;
+    
+    
+                    if (i = NBINPUTS-1) then
+                        i := 0; 
+                        else
+                        i := i+1;
+                    end if;
+    
+                end if;
+    
+            end process;
+
+/*
+# +--------------------+
+# | FINAL REPORT       |
+# |--------------------+
+# | Nb warnings = 0
+# | Nb errors   = 1
+# |
+# | Verbosity level is : note
+# |
+# | END OF SIMULATION
+#    Time: 100 us  Iteration: 0  Instance: /neuron_tb/monitor
+# +--------------------+
+*/
 end pipeline;
